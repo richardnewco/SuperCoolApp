f493437d06db61b306ca00bbc9700109481a1508
ios <ios@Richards-MacBook-Pro.local>
2016-07-12 10:04:17 +0100

    Added files for red n blue

---
+++ redAndBlue/ViewController.swift
@@ -0,0 +1,46 @@
+//
+//  ViewController.swift
+//  redAndBlue
+//
+//  Created by ios on 7/07/2016.
+//  Copyright © 2016 Ecotoonz. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    
+    @IBOutlet var redSnoopyImage: UIImageView!
+    
+   
+    @IBOutlet var blueSnoopyImage: UIImageView!
+    
+   
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    
+    
+    }
+   
+    @IBAction func redButton(sender: AnyObject) {
+    
+        redSnoopyImage .hidden = true
+    }
+
+
+    @IBAction func blueButton(sender: AnyObject) {
+        
+        blueSnoopyImage.hidden = true
+   
+    }
+   
+
+}
+
